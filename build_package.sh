00c395c6c5b7ae76b349955efff316c84bfb416d
alee-altiscale <alee@altiscale.com>
2018-06-06 07:06:41 -0700

    GREEN-241: A hack to apply downloaded JARs directly

--- build_package.sh
+++ build_package.sh
@@ -9,16 +9,63 @@ echo "ok - BUILD_BRANCH=$BUILD_BRANCH"
 echo "ok - PACKAGE_BRANCH=$PACKAGE_BRANCH"
 
 ret=0
-pushd ${WORKSPACE}/pickling
-# UNZIP_DIR="pickling-$(echo $PACKAGE_BRANCH | tr -d 'v')"
-# wget -O $PACKAGE_BRANCH.zip https://github.com/Altiscale/pickling/archive/$PACKAGE_BRANCH.zip
-# if [ -d ./$UNZIP_DIR ] ; then
-#   rm -rf ./$UNZIP_DIR
-# fi
-# unzip $PACKAGE_BRANCH.zip
-# pushd $UNZIP_DIR
-sbt package
-ret=$?
+export DATE_STRING=${DATE_STRING:-$(date -u +%Y%m%d%H%M)}
+INSTALL_DIR=${WORKSPACE}/install-dir-${DATE_STRING}
+pushd ${WORKSPACE}
+
+# pushd ${WORKSPACE}/pickling
+# sbt package
+# ret=$?
+
+# This is a hack to just package the JAR before we can build it correctly
+# hack_target should exist already with the JARs we need
+export RPM_NAME=`echo scala-pickling-${PACKAGE_BRANCH}`
+export RPM_DESCRIPTION="scala-pickling library ${PACKAGE_BRANCH}"
+
+##################
+# Packaging  RPM #
+##################
+export RPM_BUILD_DIR="${INSTALL_DIR}/usr/sap/spark/controller/"
+# Generate RPM based on where spark artifacts are placed from previous steps
+rm -rf "${RPM_BUILD_DIR}"
+mkdir --mode=0755 -p "${RPM_BUILD_DIR}"
+mkdir --mode=0755 -p "${RPM_BUILD_DIR}/lib"
+mkdir --mode=0755 -p "${RPM_BUILD_DIR}/lib_2.11"
+
+pushd hack_target
+# deploy test suite and scripts
+cp -rp scala-pickling_2.10-*.jar $RPM_BUILD_DIR/lib/
+cp -rp scala-pickling_2.11-*.jar $RPM_BUILD_DIR/lib_2.11/
+
+pushd ${RPM_DIR}
+fpm --verbose \
+--maintainer andrew.lee02@sap.com \
+--vendor SAP \
+--provides ${RPM_NAME} \
+--description "$(printf "${RPM_DESCRIPTION}")" \
+--replaces ${RPM_NAME} \
+--url "https://github.com/Altiscale/pickling" \
+--license "Proprietary" \
+--epoch 1 \
+--rpm-os linux \
+--architecture all \
+--category "Development/Libraries" \
+-s dir \
+-t rpm \
+-n ${RPM_NAME} \
+-v ${PACKAGE_BRANCH} \
+--iteration ${DATE_STRING} \
+--rpm-user root \
+--rpm-group root \
+--rpm-auto-add-directories \
+-C ${INSTALL_DIR} \
+usr
+
+if [ $? -ne 0 ] ; then
+	echo "FATAL: scala-pickling rpm build fail!"
+	popd
+	exit -1
+fi
 popd
 
-exit $ret
+exit 0
